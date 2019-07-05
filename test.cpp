0bc9d2723272b380f74317bce48e6c26b51648d5
alejandrogh97 <alejandrogoicochea@gmail.com>
2019-07-05 13:45:06 -0500

    Report changes

--- test.cpp
+++ test.cpp
@@ -13,11 +13,10 @@ TEST_CASE("Tipo int"){
 TEST_CASE("Tipo float"){
     vector<float> vectorFloat = {1.2,5.3,45.8,8.5,88899,14.9,8,6,1,92.5,106,9999,7,1,1};
     REQUIRE(buscar(vectorFloat)==88899.0f);
-
 }
 
 TEST_CASE("Tipo unsigned"){
-    vector<unsigned int> vectorFloat = {2,6,3,5,4,7,66,66,23,99,6,22,45,1,1};
-    REQUIRE(buscar(vectorFloat)==99);
+    vector<unsigned int> vectorFloat = {2,6,3,5};
+    REQUIRE(buscar(vectorFloat)==6);
 
 }
